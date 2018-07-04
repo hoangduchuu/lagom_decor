a02ad9d5270a3c1b75166cc5c82cd0e234577953
AnuBeyond <anuruddhikahettige@gmail.com>
2018-07-04 21:58:19 +0700

    create admin views

---
+++ app/controllers/admin/consultings_controller.rb
@@ -0,0 +1,2 @@
+class Admin::ConsultingsController < ApplicationController
+end
---
+++ test/controllers/admin/consultings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::ConsultingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
