c634ab1009b212d8f3af3f8062d73e99630c41d4
AnuBeyond <anuruddhikahettige@gmail.com>
2018-07-05 14:54:43 +0700

    editing

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/asset-libs/*"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/user-assets/*"].sort_by { |dir| -dir.size }
 
 %w( contacts admin/contacts ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css.scss"]
--- vendor/js/bottomslider.js
+++
@@ -1,9 +0,0 @@
-            $(document).ready(function() {
-                $("#owl-demo").owlCarousel({
-                    items: 8,
-                    lazyLoad: true,
-                    autoPlay: true,
-                    speed: 1000,
-                    pagination: false,
-                })
-            });
