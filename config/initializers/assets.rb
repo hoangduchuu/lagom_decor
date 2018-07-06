ba429fe36ecfa98c174425faa2e3baea8e4bd7ef
AnuBeyond <anuruddhikahettige@gmail.com>
2018-07-06 09:10:51 +0700

    edit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/user-assets/*"].sort_by { |dir| -dir.size }
 
-%w( user/contacts admin/contacts ).each do |controller|
+%w( user/consultings admin/consultings ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css.scss"]
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+namespace :admin do
+  	resources :consultings
+  end
+
+namespace :user do
+  	resources :consultings
+  end
 	
 end
