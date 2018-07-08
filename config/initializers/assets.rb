d18c0ef5a886ec3c549045f6ce183e6c6e1b2c09
AnuBeyond <anuruddhikahettige@gmail.com>
2018-07-09 01:16:59 +0700

    styling up admin

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/user-assets/*"].sort_by { |dir| -dir.size }
+Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/admin-assets/*"].sort_by { |dir| -dir.size }
 
 %w( user/contacts admin/contacts ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css.scss"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Rails.application.routes.draw do
+
+	namespace :user do 
+		resources :contacts
+	end
+
+	namespace :admin do 
+		resources :contacts
+	end
+
 end
