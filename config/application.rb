b5f2fde7888d45e5ca2b8f03038d7afa78a0a895
AnuBeyond <anuruddhikahettige@gmail.com>
2018-07-24 01:59:12 +0700

    some changes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module LagomDecor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    Addressable::URI.parse("http://localhost:3000/user/homes").host
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
