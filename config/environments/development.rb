f8a0c9261ef34e5979ebe00277b145e42be16f06
AnuBeyond <anuruddhikaonline@gmail.com>
2018-11-15 09:39:29 +0700

    changing mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -55,13 +55,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
     config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: "gmail.com",
+    domain: "sendgrid.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: "anu.in.vietnam@gmail.com",
-    password: "pxbbvvnpqhxwgvyx"
+    user_name: "HurryUpSoftware-Ruby",
+    password: "QhL!jkEw2o#t"
   }
 
 end
