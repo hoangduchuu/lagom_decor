412fd0cf65a172d88e7667381b46d73fcc98f752
AnuBeyond <anuruddhikaonline@gmail.com>
2018-12-23 14:35:15 +0700

    some changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => 'HAGSUICKMJGASFENR7CD',
-      :aws_secret_access_key => 'YKMnGtd0bCfWZ33F8+K6aAwBRZr84CwT6AXb7a6o3T4',
-      :region => 'sgp1',
-      :endpoint => 'https://sgp1.digitaloceanspaces.com'
+      :aws_access_key_id => 'your access key id goes here',
+      :aws_secret_access_key => 'your secret access key goes here',
+      :region => 'your region goes here', #you can choose any region
+      :endpoint => 'your endpoint goes here' #endpoint depends on the service you choose (amazon or digital ocean)
     }
-    config.fog_directory = 'rordev'
+    config.fog_directory = 'your bucket name'
     config.storage = :fog
   else
     config.storage = :file
